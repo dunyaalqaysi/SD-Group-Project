87ff81ff2c73b2818806ef9e70a0ce6dd0433c66
smleake <sethleake@hotmail.com>
2022-02-23 16:13:55 -0600

    added full states list

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -24,10 +24,13 @@ export default function Profile() {
   ) => {
     setCurrState(event.target.value);
   };
-  const states = ["TX", "AL", "AK"];
-  //   states.map(option => {
-  //       console.log(option)
-  //   })
+  const states = [
+    'AL','AK','AS','AZ','AR','CA','CO','CT','DE','DC','FM','FL','GA',
+    'GU','HI','ID','IL','IN','IA','KS','KY','LA','ME','MH','MD','MA',
+    'MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND',
+    'MP','OH','OK','OR','PW','PA','PR','RI','SC','SD','TN','TX','UT',
+    'VT','VI','VA','WA','WV','WI','WY'
+   ];
   return (
     <div>
       <Navbar />
